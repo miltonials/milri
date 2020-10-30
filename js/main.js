c42c99bc630c133010638133ef68966c9ebf0f99
Milton Barrera <miltonjosue2001@gmail.com>
2020-10-29 19:08:42 -0600

    Vista mobile completada

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-const menu = document.querySelector('.rigth-menu');
-const menu2 = document.querySelector('.left-menu');
+const rigth_menu = document.querySelector('.rigth-menu');
+const left_menu = document.querySelector('.left-menu');
+const logo = document.querySelector('.logo');
 const burgerButton = document.querySelector('#burger-menu');
 const card = document.querySelector('.aside-container');
 const tablet = window.matchMedia('screen and (max-width:812px)');
-
-console.log(menu);
 
 tablet.addListener(validation);
 
 validation(tablet); //Para que funcione el burgerButton si se inicia desde <767px
 
 function validation(event) {
-    console.log(event.matches);
     if (event.matches) {
         burgerButton.addEventListener('click', hideShow);
     }
@@ -21,19 +19,18 @@ function validation(event) {
 } 
 
 function hideShow() {
-    if (menu.classList.contains('is-active')){
-        menu.classList.remove('is-active');
+    if (rigth_menu.classList.contains('is-active')){
+        logo.classList.remove('is-active');
         burgerButton.classList.remove('is-active');
+        rigth_menu.classList.remove('is-active');
+        left_menu.classList.remove('is-active');
         card.classList.remove('is-active');
-        menu2.classList.remove('is-active');
     }
     else {
-        menu.classList.add('is-active');
-        card.classList.add('is-active');
-        menu2.classList.add('is-active');
+        logo.classList.add('is-active');
         burgerButton.classList.add('is-active');
-        console.log(menu);
-        console.log(menu2);
-        console.log(card);
+        rigth_menu.classList.add('is-active');
+        left_menu.classList.add('is-active');
+        card.classList.add('is-active');
     }
 }
