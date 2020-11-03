b2d7139cbd542e78049667c15918e5f1529ccce5
Milton Barrera <miltonjosue2001@gmail.com>
2020-11-03 17:15:52 -0600

    modals

--- js/main.js
+++ js/main.js
@@ -11,8 +11,6 @@ const rigth_menu = document.querySelector('.rigth-menu');
 const left_menu = document.querySelector('.left-menu');
 const logo = document.querySelector('.logo');
 const burgerButton = document.querySelector('#burger-menu');
-const left_menu = document.querySelector('.left-menu');
-const rigth_menu = document.querySelector('.rigth-menu');
 const card = document.querySelector('.aside-container');
 const tablet = window.matchMedia('screen and (max-width:812px)');
 
@@ -34,16 +32,16 @@ function hideShow() {
         bodyColors.classList.remove('is-active');
         logo.classList.remove('is-active');
         burgerButton.classList.remove('is-active');
-        left_menu.classList.remove('is-active');
         rigth_menu.classList.remove('is-active');
+        left_menu.classList.remove('is-active');
         card.classList.remove('is-active');
     }
     else {
         bodyColors.classList.add('is-active')
         logo.classList.add('is-active');
         burgerButton.classList.add('is-active');
-        left_menu.classList.add('is-active');
         rigth_menu.classList.add('is-active');
+        left_menu.classList.add('is-active');
         card.classList.add('is-active');
     }
 }
