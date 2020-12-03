50851e83a842a06d88fbc2baad6a6e6af82f8a88
Milton Barrera <miltonjosue2001@gmail.com>
2020-12-02 21:07:44 -0600

    ajustando el footer

--- js/footer/index.js
+++ js/footer/index.js
@@ -1,20 +1,20 @@
 const footer = () => {
-    const el = `
+  const el = `
         <section class="footer-container">
             <div class="footer-information">
-            <ul class="information-links">
-                <a href=""><li><span>Acerca de</span></li></a>
-                <a href=""><li><span>Funcionalidades</span></li></a>
-                <a href=""><li><span>¿Cómo colaborar?</span></li></a>
-                <a href="./license.html"><li><span>Licencia</span></li></a>
-            </ul>
-            <div class="social-links">
-                <span>MILRI 2020</span>
-                <span>REDES SOCIALES</span>
-            </div>
+                <ul class="information-links">
+                    <li><a href="#"><span>Acerca de</span></a></li>
+                    <li><a href="#"><span>Funcionalidades</span></a></li>
+                    <li><a href="#"><span>¿Cómo colaborar?</span></a></li>
+                    <li><a href="./license.html"><span>Licencia</span></a></li>
+                </ul>
+                <div class="social-links">
+                    <span>MILRI 2020</span>
+                    <span>REDES SOCIALES</span>
+                </div>
             </div>
         </section>
     `;
-    document.querySelector('footer').innerHTML += el;
-    }
-    footer();
+  document.querySelector("footer").innerHTML += el;
+};
+footer();
